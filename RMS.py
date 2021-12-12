9618c12960b801c6975e4a97a6e1aa3599b8189c
quilp007 <quilp007@gmail.com>
2021-12-13 01:50:45 +0900

    UI, USB temp sensor
    - add USB temp sensor
    - modify UI resolution (FHD)
    - data processing (min, max, mean, ..)

--- RMS.py
+++ RMS.py
@@ -13,8 +13,41 @@ from keysight_34461a import keysight_34461a
 from datetime import datetime
 import pandas as pd
 
-PLOT_MIN = 900
-PLOT_MAX = 1100
+import time
+import serial
+
+TEST_DATA = True    # if read data from excel
+# TEST_DATA = False # if read data from 34461a
+
+if not TEST_DATA:
+    us = serial.Serial('com4', 19200)
+
+# AT Command for USB Temperature sensor
+ATCZ = b'ATCZ\r\n'
+ATCD = b'ATCD\r\n'
+ATCMODEL = b'ATCMODEL\r\n'
+ATCVER = b'ATCVER\r\n'
+ATCC = b'ATCC\r\n'
+ATCF = b'ATCF\r\n'
+# AT Command for USB Temperature sensor
+
+READ_DELAY = 0.01
+ENABLE_BLANK_LINE = False
+
+ERROR_REF = 0.05  # 5%
+ERROR_LIMIT = 0.1   # 10%
+PLOT_MIN_MAX = 0.15  # 15%
+
+RES_REF = 33000
+
+ERROR_UPPER = RES_REF + RES_REF * ERROR_REF     # + 5%
+ERROR_LOWER = RES_REF - RES_REF * ERROR_REF     # - 5%
+
+ERROR_LIMIT_UPPER = RES_REF + RES_REF * ERROR_LIMIT     # + 10%
+ERROR_LIMIT_LOWER = RES_REF - RES_REF * ERROR_LIMIT     # - 10%
+
+PLOT_UPPER = RES_REF + RES_REF * PLOT_MIN_MAX   # + 15%
+PLOT_LOWER = RES_REF - RES_REF * PLOT_MIN_MAX   # - 15%
 
 x_size = 200
 
@@ -31,9 +64,16 @@ class THREAD_RECEIVE_Data(QThread):
     def __init__(self):
         super(THREAD_RECEIVE_Data, self).__init__()
         self.time_format = '%Y%m%d_%H%M%S'
-        self.ks_34461a = keysight_34461a(sys.argv)
+
+        if TEST_DATA:
+            self.test_data = pd.read_excel('./test_data.xlsx')
+            self.data_count = 1700
+        else:
+            self.ks_34461a = keysight_34461a(sys.argv)
+
         self.__suspend = False
         self.__exit = False
+        self.log_flag = False
 
     def run(self):
         while True:
@@ -43,12 +83,25 @@ class THREAD_RECEIVE_Data(QThread):
 
             _time = datetime.now()
             _time = _time.strftime(self.time_format)
-            read = self.ks_34461a.read()
+
+            if TEST_DATA:
+                read = self.test_data[1][self.data_count]
+                self.data_count += 1
+                if self.data_count > 5000:
+                    self.data_count = 1700
+
+                time.sleep(READ_DELAY)
+            else:
+                read = self.ks_34461a.read()
+
+            # read = RES_REF
             print(_time, ': ', read)
 
             # read = self.ks_34461a.run()
             self.intReady.emit(read)
-            self.to_excel.emit(_time, read)
+
+            if self.log_flag:
+                self.to_excel.emit(_time, read)
 
             ### Exit ###
             if self.__exit:
@@ -99,26 +152,29 @@ class qt(QMainWindow, form_class):
         self.curve = self.p6.plot(pen='y')
         self.p6.setGeometry(0, 0, x_size, 5)
 
-        self.p6.setYRange(PLOT_MIN, PLOT_MAX, padding=0)
+        self.p6.setYRange(PLOT_UPPER, PLOT_LOWER, padding=0)
         # self.p6.setMinimumHeight(1)
         # self.p6.setMaximumHeight(-1)
         # self.data = np.random.normal(size=(10, 1000))
         self.ptr = 0
 
-        self.graphWidget.nextRow()
+        # self.graphWidget.nextRow()
 
-        self.p7 = self.graphWidget.addPlot(title="Temp.")
+        self.p7 = self.graphWidget_2.addPlot(title="Temp.")
         self.curve_2 = self.p7.plot(pen='y')
 
         self.timer = QtCore.QTimer()
         self.timer.setInterval(10)
         # self.timer.timeout.connect(self.update_func_1)
-        self.timer.timeout.connect(self.plot)
+        if TEST_DATA:
+            self.timer.timeout.connect(self.sine_plot)
+        else:
+            self.timer.timeout.connect(self.usb_temp_plot)
+
         self.timer.start()
 
-        self.counter = x_size
-
-        # self.update_func_1()
+        if TEST_DATA:
+            self.counter = x_size
 
         self.first_flag = 1
 
@@ -130,16 +186,36 @@ class qt(QMainWindow, form_class):
         self.resist_data = []
         # self.writer = pd.ExcelWriter('./data.xlsx')
 
+        self.prev_data = 0
+        self.data_list = []
+
+        self.log_flag = False
+
     def to_excel_func(self, _time, data):
         tt = [_time, data]
         self.resist_data.append(tt)
         print(tt)
 
     def update_func_2(self, msg):
-        if msg > PLOT_MAX:
-            msg = PLOT_MAX
+        if msg > ERROR_LIMIT_UPPER:
+            msg = ERROR_LIMIT_UPPER
+        elif msg < ERROR_LIMIT_LOWER:
+            msg = ERROR_LIMIT_LOWER
 
         print(msg)
+
+        if msg != ERROR_LIMIT_UPPER:
+            self.data_list.append(msg)
+            self.prev_data = msg
+            return
+        elif self.prev_data != ERROR_LIMIT_UPPER:
+            mean_data = np.mean(self.data_list)
+            self.data_list = []
+            print('mean: ', mean_data)
+            self.prev_data = msg
+            msg = mean_data
+        elif (self.prev_data == ERROR_LIMIT_UPPER and msg == ERROR_LIMIT_UPPER) and not ENABLE_BLANK_LINE:
+            return
 
         if self.first_flag == 1:
             self.y1 = np.full(len(self.data), msg)
@@ -152,13 +228,14 @@ class qt(QMainWindow, form_class):
 
         self.curve.setData(self.y1)
 
-        self.lcdNum_T_PV_CH1.display("{:.1f}".format(msg))
+        msg = msg / 1000;   # convert k ohm
+        self.lcdNum_T_PV_CH1.display("{:.2f}".format(msg))
 
         if self.ptr == 0:
             self.p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
         self.ptr += 1
 
-    def plot(self):
+    def sine_plot(self):
         # self.g_plotWidget.plot(hour, temperature)
         # curve = self.graphWidget_2.plot(pen='y')
         self.y2 = np.roll(self.y2, -1)
@@ -172,16 +249,36 @@ class qt(QMainWindow, form_class):
 
         self.counter += 1
 
+    def usb_temp_plot(self):
+        us.write(b'ATCD\r\n')
+        line = us.readline().decode('utf-8')
+        # print(line)
+
+        self.y2 = np.roll(self.y2, -1)
+
+        temp_data = line.split(' ')[1].split(',')[0]
+        temp_data = float(temp_data)
+        self.y2[-1] = temp_data
+        self.curve_2.setData(self.y2)
+
+        self.lcdNum_T_SV_CH1.display("{:.1f}".format(temp_data))
+
     def btn_34461a(self, button):
         if button == self.btn_start:
             self.thread_rcv_data.myResume()
+            self.thread_rcv_data.log_flag = True
         elif button == self.btn_stop:
-            self.thread_rcv_data.mySuspend()
+            self.thread_rcv_data.log_flag = False
+            # self.thread_rcv_data.mySuspend()
             df1 = pd.DataFrame(self.resist_data)
             _time = datetime.now()
             _time = _time.strftime(self.thread_rcv_data.time_format)
-            df1.to_excel(_time+'.xlsx')
+
+            with pd.ExcelWriter(_time+'.xlsx') as writer:
+                df1.to_excel(writer, _time+'.xlsx')
+
             self.resist_data = []
+
         elif button == self.btn_close:
             self.thread_rcv_data.close()
 
