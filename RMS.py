8c4541fc7fab2a787291901ea50a9208927d5bba
quilp007 <quilp007@gmail.com>
2022-01-05 05:45:14 +0900

    modify graph and add 1 sheet parallel resistance

--- RMS.py
+++ RMS.py
@@ -25,16 +25,22 @@ import serial
 
 # RES_REF = 33000
 RES_REF = 5000
+P_RES_REF = 310
 
 LINE_NUM = 16  # thermal film line
 ROW_COUNT = 30  # limit: 30
 ROW_COUNT_2 = 3  # limit: 3
+
+P_ERROR_REF = 0.05  # 5%
+P_ERROR_LIMIT = 0.125  # 12.5%
+P_PLOT_MIN_MAX = 0.15  # 15%
 
 ERROR_REF = 0.05  # 5%
 # ERROR_LIMIT = 0.1     # 10%
 ERROR_LIMIT = 0.125  # 12.5%
 PLOT_MIN_MAX = 0.15  # 15%
 
+mean_plot_x_size = 100  # graph's x size
 x_size = 200  # graph's x size
 
 # config for keysight 34461a
@@ -42,8 +48,8 @@ display = True  # 34461a display On(True)/Off(False)
 res_range = 100000  # 34461a range (ohm, not k ohm)
 COM_PORT = 'com4'
 
-READ_DELAY = 0.01
-# READ_DELAY = 0.0005
+# READ_DELAY = 0.01
+READ_DELAY = 0.005
 ENABLE_BLANK_LINE = False
 BLANK_DATA_COUNT = 20
 # ------------------------------------------------------------------------------
@@ -62,6 +68,15 @@ ATCVER = b'ATCVER\r\n'
 ATCC = b'ATCC\r\n'
 ATCF = b'ATCF\r\n'
 # AT Command for USB Temperature sensor
+
+P_ERROR_UPPER = P_RES_REF + P_RES_REF * P_ERROR_REF  # + 5%
+P_ERROR_LOWER = P_RES_REF - P_RES_REF * P_ERROR_REF  # - 5%
+
+P_ERROR_LIMIT_UPPER = P_RES_REF + P_RES_REF * P_ERROR_LIMIT  # + 10%
+P_ERROR_LIMIT_LOWER = P_RES_REF - P_RES_REF * P_ERROR_LIMIT  # - 10%
+
+P_PLOT_UPPER = P_RES_REF + P_RES_REF * P_PLOT_MIN_MAX  # + 15%
+P_PLOT_LOWER = P_RES_REF - P_RES_REF * P_PLOT_MIN_MAX  # - 15%
 
 
 ERROR_UPPER = RES_REF + RES_REF * ERROR_REF  # + 5%
@@ -177,66 +192,65 @@ class qt(QMainWindow, form_class):
         self.data = np.linspace(-np.pi, np.pi, x_size)
         self.y1_1 = np.zeros(len(self.data))
         self.y1_2 = np.zeros(len(self.data))
-        self.y2 = np.sin(self.data)
+        # self.y2 = np.sin(self.data)
+        self.y2 = np.zeros(mean_plot_x_size)
 
         # self.plot(self.data, self.y1_1)
 
         # table Widget ------------------------------------------------------------------
         self.tableWidget.setRowCount(ROW_COUNT)
-        self.tableWidget.setColumnCount(LINE_NUM + 2)  # MEAN, parallel resistance
+        self.tableWidget.setColumnCount(LINE_NUM + 3)  # MEAN, parallel resistance
         # self.tableWidget.setColumnWidth(0, self.tableWidget.columnWidth()/10)
         self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.tableWidget.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.tableWidget.setHorizontalHeaderItem(LINE_NUM, QTableWidgetItem('MEAN'))
-        self.tableWidget.setHorizontalHeaderItem(LINE_NUM + 1, QTableWidgetItem('P. RES'))
+        self.tableWidget.setHorizontalHeaderItem(LINE_NUM + 1, QTableWidgetItem('1S. P. RES'))
+        self.tableWidget.setHorizontalHeaderItem(LINE_NUM + 2, QTableWidgetItem('2S. P. RES'))
 
         # table Widget 2-----------------------------------------------------------------
         self.tableWidget_2.setRowCount(ROW_COUNT_2)
-        self.tableWidget_2.setColumnCount(LINE_NUM + 2)  # MEAN, parallel resistance
+        self.tableWidget_2.setColumnCount(LINE_NUM + 3)  # MEAN, parallel resistance
         # self.tableWidget_2.setColumnWidth(0, self.tableWidget.columnWidth()/10)
         self.tableWidget_2.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.tableWidget_2.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.tableWidget_2.setHorizontalHeaderItem(LINE_NUM, QTableWidgetItem('MEAN'))
-        self.tableWidget_2.setHorizontalHeaderItem(LINE_NUM + 1, QTableWidgetItem('P. RES'))
+        self.tableWidget_2.setHorizontalHeaderItem(LINE_NUM + 1, QTableWidgetItem('1S. P. RES'))
+        self.tableWidget_2.setHorizontalHeaderItem(LINE_NUM + 2, QTableWidgetItem('2S. P. RES'))
 
         # Updating Plot
-        self.p6 = self.graphWidget.addPlot(title="Res")
+        self.p6 = self.graphWidget_2.addPlot(title="Res")
         self.curve1_1 = self.p6.plot(pen='g')
         self.curve1_2 = self.p6.plot(pen='r')
         self.p6.setGeometry(0, 0, x_size, 5)
 
         self.p6.setYRange(PLOT_UPPER, PLOT_LOWER, padding=0)
 
-        ERROR_LOWER_line = pg.InfiniteLine(angle=0, movable=True, pen='y')
-        ERROR_LOWER_line.setValue(ERROR_LOWER)
-        self.p6.addItem(ERROR_LOWER_line, ignoreBounds=True)
-
-        ERROR_UPPER_line = pg.InfiniteLine(angle=0, movable=True, pen='y')
-        ERROR_UPPER_line.setValue(ERROR_UPPER)
-        self.p6.addItem(ERROR_UPPER_line, ignoreBounds=True)
-
-        ERROR_LIMIT_LOWER_line = pg.InfiniteLine(angle=0, movable=True, pen='r')
-        ERROR_LIMIT_LOWER_line.setValue(ERROR_LIMIT_LOWER)
-        self.p6.addItem(ERROR_LIMIT_LOWER_line, ignoreBounds=True)
-
-        ERROR_LIMIT_UPPER_line = pg.InfiniteLine(angle=0, movable=True, pen='r')
-        ERROR_LIMIT_UPPER_line.setValue(ERROR_LIMIT_UPPER)
-        self.p6.addItem(ERROR_LIMIT_UPPER_line, ignoreBounds=True)
+        self.drawLine(self.p6, ERROR_LOWER, 'y')
+        self.drawLine(self.p6, ERROR_UPPER, 'y')
+        self.drawLine(self.p6, ERROR_LIMIT_LOWER, 'r')
+        self.drawLine(self.p6, ERROR_LIMIT_UPPER, 'r')
 
         # self.graphWidget.nextRow()
 
-        self.p7 = self.graphWidget_2.addPlot(title="Temp.")
+        self.p7 = self.graphWidget.addPlot(title="Temp.")
         self.curve2 = self.p7.plot(pen='y')
 
-        self.p7.setYRange(0, 40, padding=0)
+        self.p7.setYRange(P_PLOT_UPPER, P_PLOT_LOWER, padding=0)
+
+        self.drawLine(self.p7, P_ERROR_LOWER, 'y')
+        self.drawLine(self.p7, P_ERROR_UPPER, 'y')
+        self.drawLine(self.p7, P_ERROR_LIMIT_LOWER, 'r')
+        self.drawLine(self.p7, P_ERROR_LIMIT_UPPER, 'r')
 
         self.timer = QtCore.QTimer()
         self.timer.setInterval(10)
         # self.timer.timeout.connect(self.update_func_1)
-        if TEST_DATA:
-            self.timer.timeout.connect(self.sine_plot)
-        else:
-            self.timer.timeout.connect(self.usb_temp_plot)
+
+        # if TEST_DATA:
+        #     self.timer.timeout.connect(self.mean_value_plot)
+        #     # self.timer.timeout.connect(self.sine_plot)
+        # else:
+        #     self.timer.timeout.connect(self.usb_temp_plot)
 
         self.timer.start()
 
@@ -261,7 +275,14 @@ class qt(QMainWindow, form_class):
         self.log_flag = False
         self.sheet_blank_flag = False
 
+        self.prev_1s_p_res = 0
+
         self.main_button_function(self.btn_main)
+
+    def drawLine(self, plot_name, val, color):
+        line = pg.InfiniteLine(angle=0, movable=True, pen=color)
+        line.setValue(val)
+        plot_name.addItem(line, ignoreBounds=True)
 
     def loadParam(self):
         global LINE_NUM
@@ -282,69 +303,6 @@ class qt(QMainWindow, form_class):
         self.resist_data.append(tt)
         print(tt)
 
-    def update_func_1_test(self, msg):
-        global ptr, state
-        if msg > ERROR_LIMIT_UPPER:
-            msg = ERROR_LIMIT_UPPER
-        elif msg < ERROR_LIMIT_LOWER:
-            msg = ERROR_LIMIT_LOWER
-
-        print(msg)
-
-        if msg != ERROR_LIMIT_UPPER:
-            self.sheet_blank_flag = False
-            self.blank_count = 0
-            self.data_list.append(msg)
-            self.prev_data = msg
-            return
-        else:  # msg == ERROR_LIMIT_UPPER
-            if self.prev_data != ERROR_LIMIT_UPPER:  # rising edge
-                self.blank_count += 1
-                mean_data = np.mean(self.data_list)
-                self.data_list = []
-                print('mean: ', mean_data)
-                msg = mean_data
-                mean_data = mean_data.round(2)
-                self.line_data.append(mean_data)
-            elif self.prev_data == ERROR_LIMIT_UPPER:  # BLANK LEVEL
-                if self.blank_count > BLANK_DATA_COUNT and self.sheet_blank_flag == False:
-                    self.sheet_blank_flag = True
-                    ptr = 0
-                    p_r_sum = 0
-                    self.line_data.append(np.nanmean(self.line_data).round(2))
-                    for idx in range(LINE_NUM):
-                        p_r_sum += 1 / self.line_data[idx]
-
-                    p_r_sum = 1 / p_r_sum
-                    self.line_data.append(p_r_sum.round(2))
-
-                    print(self.line_data)
-                    self.tableWidget.removeRow(ROW_COUNT - 1)
-                    self.tableWidget.insertRow(0)
-                    self.setTableWidgetData(self.line_data)
-                    self.line_data = []
-                    self.blank_count = 0
-
-            self.prev_data = msg
-
-        # self.y1_1 = np.roll(self.y1_1, -1)
-
-        # self.y1_1[-1] = msg
-        # self.curve1_1.setData(self.y1_1)
-
-        if ptr == 0:
-            self.y1_1 = self.y1_2[:]
-            self.curve1_1.setData(self.y1_1)
-            self.y1_2 = np.zeros(x_size)
-
-        self.y1_2[ptr] = msg
-        ptr += 1
-        self.curve1_2.setData(self.y1_2)
-
-        if msg != ERROR_LIMIT_UPPER or msg != ERROR_LIMIT_LOWER:
-            msg = msg / 1000  # convert k ohm
-            self.lcdNum_T_PV_CH1.display("{:.2f}".format(msg))
-
     def update_func_1(self, msg):
         global ptr
         if msg > ERROR_LIMIT_UPPER:
@@ -355,12 +313,12 @@ class qt(QMainWindow, form_class):
         print('34661A: ', msg)
 
         # data filter
-        if msg != ERROR_LIMIT_UPPER:
+        if msg != ERROR_LIMIT_UPPER:                # line data received
             self.blank_count = 0
             self.data_list.append(msg)
             self.prev_data = msg
             return
-        elif self.prev_data != ERROR_LIMIT_UPPER:
+        elif self.prev_data != ERROR_LIMIT_UPPER:   # blank area
             mean_data = np.mean(self.data_list)
             self.data_list = []
             print('mean: ', mean_data)
@@ -370,16 +328,25 @@ class qt(QMainWindow, form_class):
             self.line_data.append(mean_data)
         elif (self.prev_data == ERROR_LIMIT_UPPER and msg == ERROR_LIMIT_UPPER) and not ENABLE_BLANK_LINE:
             self.blank_count += 1
-            if self.blank_count > 20:
+            # if self.blank_count > 20:
+            if self.blank_count > BLANK_DATA_COUNT:
                 ptr = 0
                 p_r_sum = 0
-                self.line_data.append(np.nanmean(self.line_data).round(2))
+                mean_value = np.nanmean(self.line_data).round(2)
+                # self.line_data.append(np.nanmean(self.line_data).round(2))
+                self.line_data.append(mean_value)
+                self.lcdNum_line_res.display(str(np.round(mean_value/1000, 1)))
+
+                # parallel resist
                 for idx in range(LINE_NUM):
                     p_r_sum += (1 / self.line_data[idx])
 
                 p_r_sum = (1 / p_r_sum)
+                self.lcdNum_1sheet_p_res.display(str(np.round(p_r_sum/1000, 1)))
+                two_s_p_res = (self.prev_1s_p_res + p_r_sum) /2
                 # self.line_data.append(p_r_sum.round(2))
                 self.line_data[LINE_NUM + 1] = p_r_sum.round(2)
+                self.line_data[LINE_NUM + 2] = two_s_p_res.round(2)
 
                 print(self.line_data, ' length: ', len(self.line_data))
                 self.tableWidget.removeRow(ROW_COUNT - 1)
@@ -392,6 +359,8 @@ class qt(QMainWindow, form_class):
 
                 self.line_data = []
                 self.blank_count = 0
+
+                self.mean_value_plot(p_r_sum)
 
         # self.y1_1 = np.roll(self.y1_1, -1)
 
@@ -413,63 +382,18 @@ class qt(QMainWindow, form_class):
             msg = msg / 1000  # convert k ohm
             self.lcdNum_T_PV_CH1.display("{:.2f}".format(msg))
 
-    def update_func_2(self, msg):
-        if msg > ERROR_LIMIT_UPPER:
-            msg = ERROR_LIMIT_UPPER
-        elif msg < ERROR_LIMIT_LOWER:
-            msg = ERROR_LIMIT_LOWER
-
-        print(msg)
-
-        # data filter
-        if msg != ERROR_LIMIT_UPPER:
-            self.blank_count = 0
-            self.data_list.append(msg)
-            self.prev_data = msg
-            return
-        elif self.prev_data != ERROR_LIMIT_UPPER:
-            mean_data = np.mean(self.data_list)
-            self.data_list = []
-            print('mean: ', mean_data)
-            self.prev_data = msg
-            msg = mean_data
-            mean_data = mean_data.round(2)
-            self.line_data.append(mean_data)
-        elif (self.prev_data == ERROR_LIMIT_UPPER and msg == ERROR_LIMIT_UPPER) and not ENABLE_BLANK_LINE:
-            self.blank_count += 1
-            if self.blank_count > 20:
-                p_r_sum = 0
-                self.line_data.append(np.nanmean(self.line_data).round(2))
-                for idx in range(LINE_NUM):
-                    p_r_sum += 1 / self.line_data[idx]
-
-                p_r_sum = 1 / p_r_sum
-                self.line_data.append(p_r_sum.round(2))
-
-                print(self.line_data)
-                self.tableWidget.removeRow(ROW_COUNT - 1)
-                self.tableWidget.insertRow(0)
-                self.setTableWidgetData(self.line_data)
-                self.line_data = []
-                self.blank_count = 0
-
-        # elif (self.prev_data == ERROR_LIMIT_UPPER and msg == ERROR_LIMIT_UPPER) and not ENABLE_BLANK_LINE:
-        #     return
-
-        self.y1_1 = np.roll(self.y1_1, -1)
-
-        self.y1_1[-1] = msg
-
-        self.curve1_1.setData(self.y1_1)
-
-        msg = msg / 1000  # convert k ohm
-        self.lcdNum_T_PV_CH1.display("{:.2f}".format(msg))
-
     def setTableWidgetData(self, line_data, tableWidget):
         for idx in range(0, LINE_NUM + 2):
             tableWidget.setItem(0, idx, QTableWidgetItem(str(line_data[idx])))
 
         # self.tableWidget.setItem(0, LINE_NUM, QTableWidgetItem(str(line_data[-1])))
+
+    def mean_value_plot(self, mean_value):
+        # self.g_plotWidget.plot(hour, temperature)
+        # curve = self.graphWidget_2.plot(pen='y')
+        self.y2 = np.roll(self.y2, -1)
+        self.y2[-1] = mean_value
+        self.curve2.setData(self.y2)
 
     def sine_plot(self):
         # self.g_plotWidget.plot(hour, temperature)
